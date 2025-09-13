ad2ad6aa67821aea3db28ff9e687a7e5f5c4e50b
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-13 12:44:18 +0100

    cleared index.html and style.css

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,7 @@
 // Toggle light/dark theme
 const themeToggle = document.getElementById('themeToggle')
+const jobTitle = document.getElementById('jobTitle')
+
 themeToggle.addEventListener('click', () => {
     const emojis = ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘']
     const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
@@ -32,3 +34,28 @@ themeToggle.addEventListener('click', () => {
 
     document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
 });
+
+if (jobTitle) {
+    const jobList = 'EDITOR'
+
+    setInterval(() => {
+        jobTitle.innerHTML = ''
+        for(let i = 0; i < jobList.length; i++) {
+            setTimeout(() => {
+                jobTitle.innerHTML += jobList[i]
+            }, i * 200)
+        
+        }
+
+        // jobTitle.innerHTML = jobList
+
+        // setTimeout(() => {
+        //     jobTitle.innerHTML = jobList
+        // }, jobList.length * 500)
+
+
+            
+    }, jobList.length * 500)
+    
+    
+}
